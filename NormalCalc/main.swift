03fa19f9e1c8ecc382487dd9c557070267a23b05
Josh Malters <frostymalts@gmail.com>
2015-10-12 16:41:11 -0700

    Initial Commit

---
+++ NormalCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NormalCalc
+//
+//  Created by Joshua Malters on 10/12/15.
+//  Copyright © 2015 Joshua Malters. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
